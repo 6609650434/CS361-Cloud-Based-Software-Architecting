bc61ff01b93f7b058b1c296b1d34659b7ba44bbf
noppawat jamjumrus <noppawat.jam@dome.tu.ac.th>
2025-10-24 14:16:17 +0700

    Update login and add login.js

---
+++ js/login.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('loginForm');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    // --- ตัวอย่างบัญชีทดลอง ---
+    const userAccount = {
+        email: "user@dome.tu.ac.th",
+        password: "1234"
+    };
+
+    const adminAccount = {
+        email: "admin@dome.tu.ac.th",
+        password: "1234"
+    };
+
+    // --- ตรวจสอบอีเมลและรหัสผ่าน ---
+    if (email === userAccount.email && password === userAccount.password) {
+        alert("Welcome, User!");
+        window.location.href = "user/user-home.html";
+    }
+    else if (email === adminAccount.email && password === adminAccount.password) {
+        alert("Welcome, Admin!");
+        window.location.href = "admin/admin-home.html";
+    }
+    else {
+        alert("Invalid email or password. Try again.");
+    }
+});
